b39004e2d27d2991e9b65a75da4b8a9a8de90cd3
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-11-02 23:57:31 -0700

    add

--- app.js
+++ app.js
@@ -31,30 +31,6 @@ const studentApp = {
       ...newStudents,
     };
   },
-  handleUpdate: (id) => {
-    const studentState = [...studentApp.state.students];
-    const student = studentState.find((x) => x.id === id);
-    if (student) {
-      studentApp.state.newStudent = student;
-      studentApp.handleStudentForm(student);
-    }
-  },
-  handlePreview: (id) => {
-    const studentState = [...studentApp.state.students];
-    const student = studentState.find((x) => x.id === id);
-    if (student) {
-      studentApp.state.newStudent = student;
-      const previewContent = `
-          <div class="preview">
-            <p>Name: ${student.firstName}  </p>
-            <p>Class: ${student.class}</p>
-            <p>About: ${student.about}
-          </div>
-      `;
-      preview.style.border = `1px solid #eee`;
-      preview.innerHTML = previewContent;
-    }
-  },
 
   handleStudentForm: (student) => {
 
@@ -62,29 +38,10 @@ const studentApp = {
       <div class="modal-content" onclick="studentApp.stopPropagation(event)">
       <div class="close" onclick="studentApp.closeModal()">&times</div>
 
-      <h3>${student ? "Update" : "Add"} Student Form</h3>
-
-      <form onkeyup="studentApp.handleChange(event)" onsubmit="studentApp.handleStudent(event)">
-      <input class="input" value="${
-        student ? student.firstName : ""
-      }" name="firstName" placeholder="Enter first name"/>
-        <input class="input" value="${
-          student ? student.class : ""
-        }" name="class" placeholder="Enter class"/>
-        <input class="input" name="picture" type="file"/>
-        <button type="submit">${student ? "Update" : "Add"}</button>
-        </form>
         </div>
         `;
     modal.style.display = "flex";
     modal.innerHTML = form;
-  },
-  closeModal: () => {
-    modal.style.display = "none";
-    studentApp.state.newStudent = {
-      firstName: "",
-      class: "",
-    };
   },
   render: () => {
     studentList.innerHTML = "";
@@ -94,11 +51,6 @@ const studentApp = {
                 <div class="list-item">${item.id}</div>
                 <div class="list-item">${item.firstName}</div>
                 <div class="list-item">${item.class}</div>
-                <div>
-                    <button onclick="studentApp.handlePreview(${item.id})">View</button>
-                    <button onclick="studentApp.handleUpdate(${item.id})">Edit</button>
-                    <button onclick="studentApp.openDeleteModal(${item.id})">Delete</button>
-                </div>
             </div>
         `;
       studentList.innerHTML += student;
@@ -109,3 +61,4 @@ const studentApp = {
   },
 };
 studentApp.init();
+
